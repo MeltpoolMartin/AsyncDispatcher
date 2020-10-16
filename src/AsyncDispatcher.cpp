e933807561a424187ff0f181a2ee14d93ee191b2
MeltpoolMartin <martinwachter@gmx.net>
2020-10-16 22:08:13 +0200

    This commit is the initial implementaion of AsyncDispatcher with one thread

---
+++ src/AsyncDispatcher.cpp
@@ -0,0 +1,37 @@
+#include "AsyncDispatcher/AsyncDispatcher.hpp"
+
+AsyncDispatcher::AsyncDispatcher() : m_stop(false) {
+  m_threadWorker = std::thread([this] { workerThread(); });
+}
+
+AsyncDispatcher::~AsyncDispatcher() {
+  stop();
+  if (m_threadWorker.joinable()) {
+    m_threadWorker.join();
+  }
+}
+
+void AsyncDispatcher::stop() {
+  m_stop = true;
+  m_condVarWorker.notify_all();  // check if notify_one() is sufficient
+}
+
+void AsyncDispatcher::dispatchFunc(std::function<void()> func) {
+  std::lock_guard<std::mutex> lock(m_mutexWorker);
+  m_queue.push(std::move(func));  // check with emplace
+  m_condVarWorker.notify_all();
+}
+
+void AsyncDispatcher::workerThread() {
+  std::unique_lock<std::mutex> lock(m_mutexWorker);
+
+  do {
+    m_condVarWorker.wait(lock, [this] { return m_stop || m_queue.size(); });
+    if (m_queue.size()) {
+      auto func = std::move(m_queue.front());
+      lock.unlock();
+      func();
+      lock.lock();
+    }
+  } while (m_stop != false);
+}
