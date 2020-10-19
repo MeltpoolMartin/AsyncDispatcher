8bad8fcebb4660b2d881358b379ba38f106dd71f
MeltpoolMartin <martinwachter@gmx.net>
2020-10-19 09:50:45 +0200

    This commit removes the print to console output

--- src/AsyncDispatcher.cpp
+++ src/AsyncDispatcher.cpp
@@ -1,7 +1,5 @@
 #include "AsyncDispatcher/AsyncDispatcher.hpp"
 
-#include <iostream>
-
 AsyncDispatcher::AsyncDispatcher() : m_stop(false), m_forcedSync(false) {
   m_threadWorker = std::thread([this] { workerThread(); });
 }
@@ -15,7 +13,6 @@ AsyncDispatcher::~AsyncDispatcher() {
 
 void AsyncDispatcher::stop() {
   m_stop = true;
-  std::cout << "Resync after leaving scope of AsyncDispatcher obj" << std::endl;
   m_condVarWorker.notify_all();  // check if notify_one() is sufficient
 }
 
@@ -30,13 +27,11 @@ void AsyncDispatcher::workerThread() {
     std::unique_lock<std::mutex> lock(m_mutexWorker);
     m_condVarWorker.wait(lock, [this] { return m_stop || m_queue.size() > 0; });
     if (m_queue.size() > 0) {
-      std::cout << "Processing " << m_queue.size() << "task(s)" << std::endl;
       auto func = std::move(m_queue.front());
       m_queue.pop();
       func();
     }
     if (m_queue.size() == 0) {
-      std::cout << "Thread " << std::this_thread::get_id() << " triggers resync" << std::endl;
       m_forcedSync = true;
       m_condVarWorker.notify_all();
     }
@@ -45,8 +40,6 @@ void AsyncDispatcher::workerThread() {
 
 void AsyncDispatcher::resyncWithFunc() {
   std::unique_lock<std::mutex> lock(m_mutexWorker);
-  std::cout << "Thread " << std::this_thread::get_id() << " waits for resync" << std::endl;
   m_condVarWorker.wait(lock, [this] { return m_forcedSync; });
   m_forcedSync = false;
-  std::cout << "Thread " << std::this_thread::get_id() << " was synced" << std::endl;
 }
--- test/testAsyncDispatcher.cpp
+++ test/testAsyncDispatcher.cpp
@@ -3,17 +3,15 @@
 #include "AsyncDispatcher/AsyncDispatcher.hpp"
 #include "gtest/gtest.h"
 
-TEST(AsyncDispatcherTestSuite, Constructor_CheckConstructionAndDestruction) {
+TEST(AsyncDispatcherTestSuite,
+     ResyncWithFunc_DispatchFunctionsAndResyncAfterExecution_CanExecuteTasksAndResync) {
   AsyncDispatcher asyncDispatcher;
   auto task = [](int taskNumber) {
     std::cout << "task " << taskNumber << " with thread ID " << std::this_thread::get_id()
               << std::endl;
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
   };
   asyncDispatcher.dispatchFunc([&task] { task(1); });
   asyncDispatcher.dispatchFunc([&task] { task(2); });
   asyncDispatcher.resyncWithFunc();
-  asyncDispatcher.dispatchFunc([&task] { task(3); });
-  EXPECT_TRUE(true);
-  std::cout << "Main function leaves scope" << std::endl;
 }
